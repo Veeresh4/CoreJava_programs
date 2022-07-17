5bb9d5b848920a2098e948f4fbc06ad3653c636e
Veeresh <Veeresh@DESKTOP-RF8GFGH>
2022-07-17 11:19:18 +0530

    commit

---
+++ case-study2/src/banking/mmbankfactory.java
@@ -0,0 +1,30 @@
+package banking;
+
+public class mmbankfactory implements bankfactory {
+
+	
+
+	public void getcurrentaccount(int accno, String accname, float accbal, float isSalaried) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public savingacc getnewsavingaccount(int accno, String accname, float accbal, float isSalaried) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public currentacc getnormalaccount(int accno, String accname, float charges, float creditlimit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public mmsavingacc getnewsavingaccount(int accno, String accname, float charges, boolean isSalaried) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
